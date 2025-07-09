fe23b877031b883c5f88d027ac69fa3225e1c200
Gabriel Gomes <gabrielgomeseleven@gmail.com>
2025-07-09 09:13:05 -0300

    final

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function loadCard() {
     const cardone = document.getElementById("card-1");
     const cardtwo = document.getElementById("card-2");
+    const buttons = document.querySelectorAll(".buttons");
     cardtwo.style.display = "block";
     cardone.style.display = "none";
+} 
+function checkButton(){
+    let buttonActive = false;
+
+    buttons.forEach(submit => {
+        if (submit.classList.contains("active")) {
+            buttonActive = true;
+            return; 
+        }});
+
+        if (!buttonActive) {
+            alert("Select an option");
+            return;
+        }
+        else{
+            loadCard(); 
+        }
 }
 
 const buttons = document.querySelectorAll(".buttons");
@@ -11,9 +29,6 @@ buttons.forEach(button => {
   button.addEventListener('click', () => {
     buttons.forEach(bnt => bnt.classList.remove("active")); 
     button.classList.add("active");
-    
-    button.classList.remove("animate__animated", "animate__pulse");
-    button.classList.add("animate__animated", "animate__pulse");
 
     const mensage = button.getAttribute("data-mensage");
     text.innerHTML = mensage;
